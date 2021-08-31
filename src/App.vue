ea3789af09cc7bd11febbba4579a6fc00c76c40a
Tima <bredos11@yandex.ru>
2021-08-31 12:33:47 +0300

    added less-loader

--- src/App.vue
+++ src/App.vue
@@ -65,36 +65,10 @@ export default {
 };
 </script>
 
-<style>
-.product-list {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  list-style: none;
-}
-
-.product-item {
-  width: calc(100% / 3 - 15px);
-  margin-bottom: 15px;
-}
-
-.product-item:not(:nth-child(3n)) {
-  margin-right: 15px;
-}
-
-.product-image-wrapper {
-  display: flex;
-  align-items: center;
-}
-
-.product-image {
-  width: 100%;
-  max-height: 250px;
-  object-fit: contain;
-}
-
-.product-title {
-  margin-top: 5px;
-  margin-bottom: 5px;
+<style lang="less">
+.pagination {
+  &__link {
+    color: red;
+  }
 }
 </style>
